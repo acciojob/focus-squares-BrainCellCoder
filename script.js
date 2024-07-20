fa9364bb0fa8780c78f59af6ca08b6c9bf0b10a0
BrainCellCoder <70462882+BrainCellCoder@users.noreply.github.com>
2024-07-20 23:20:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//your JS code here. If required.
+function handleMouseOver(event) {
+	const hoveredSquareId = event.target.id;
+	document.querySelectorAll('.square').forEach(square => {
+		if (square.id !== hoveredSquareId) {
+			square.style.backgroundColor = '#6F4E37'; 
+		}
+	});
+}
+function handleMouseOut() {
+	document.querySelectorAll('.square').forEach(square => {
+		square.style.backgroundColor = '#E6E6FA';
+	});
+}
+document.querySelectorAll('.square').forEach(square => {
+	square.addEventListener('mouseover', handleMouseOver);
+	square.addEventListener('mouseout', handleMouseOut);
+});
